b4c9d6f7ca9203c16bb91bcb00ee9dcd5207abdb
Daniela Correia <93346248+danifc10@users.noreply.github.com>
2022-01-12 23:07:06 +0000

    Add files via upload

---
+++ logica.js
@@ -0,0 +1,11 @@
+
+
+
+document.getElementById("zonas").style.display = "none";
+
+document.getElementById("btnCalculo").addEventListener('click',function () {
+    document.getElementById("zonas").style.display = "block";
+})
+
+
+
